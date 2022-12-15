5afaf98404a1a25649c3ce1887db03f5ec1a52ea
Taylor <63395324+Pastedd@users.noreply.github.com>
2022-12-15 15:17:17 +0900

    Update state_machine.v

--- FPGA Elevator (fixed)/state_machine.v
+++ FPGA Elevator (fixed)/state_machine.v
@@ -20,20 +20,20 @@ always @ (posedge clk) begin
     else begin
         case(state)
             state_idle: begin
-                if(btn_stable_shot[0])      state   <=  state_floor1;
-                else if(btn_stable_shot[1]) state   <=  state_going_to_2;
+                if(btn_stable_shot[0])      state   <=  state_floor1;                               //btn0 --> go to floor 1
+                else if(btn_stable_shot[1]) state   <=  state_going_to_2;                           //btn1 --> go to floor 2
             end
             state_floor1: begin                
-                if(btn_stable_shot[1] & (counting_value == 0))      state   <=  state_going_to_2;
+                if(btn_stable_shot[1] & (counting_value == 0))      state   <=  state_going_to_2;   //btn1 --> go to floor 2
             end
             state_floor2: begin
-                if(btn_stable_shot[0] & (counting_value == 0))      state   <=  state_going_to_1;
+                if(btn_stable_shot[0] & (counting_value == 0))      state   <=  state_going_to_1;   //btn0 --> go to floor 1
             end
             state_going_to_1: begin
-                if(counting_value == 0)     state   <=  state_floor1;
+                if(counting_value == 0)     state   <=  state_floor1;                               //take 5 seconds until going to floor 1
             end
             state_going_to_2: begin
-                if(counting_value == 0)     state   <=  state_floor2;
+                if(counting_value == 0)     state   <=  state_floor2;                               //take 5 seconds until going to floor 2
             end
             default: state  <=  state_idle;
         endcase
